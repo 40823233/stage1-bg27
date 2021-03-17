3acffb2a5f7089c67d26d2358344cfc713210139
40823233 <40823233@gm.nfu.edu.tw>
2021-03-17 20:45:18 +0800

    3/17stage1-bg27報告上傳-1

--- init.py
+++ init.py
@@ -11,8 +11,8 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "stage1-bg27"
-    #ip = "127.0.0.1"
-    ip = "::"
+    ip = "127.0.0.1"
+    #ip = "::"
     port = 9443
     def __init__(self):
         # hope to create downloads and images directories　
